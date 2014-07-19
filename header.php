b627436f59d8567f036b06b1832af98eb7238b06
Yager Guo <yager.guo@gmail.com>
2014-07-19 20:51:25 +0800

    显示所有分类

--- header.php
+++ header.php
@@ -3,3 +3,7 @@
         <title><?php bloginfo('name'); ?></title>
     </head>
     <body>
+
+        <h2>所有分类</h2>
+        <?php wp_list_categories(array('hide_empty' => false)) ?>
+        <hr />
