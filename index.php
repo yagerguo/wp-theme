661eda778e284651072c80926896d1ce23ea35c1
Yager Guo <yager.guo@gmail.com>
2014-07-19 17:29:29 +0800

    输出文章信息

--- index.php
+++ index.php
@@ -4,4 +4,10 @@ echo 'Hello, welcome to ';
 bloginfo('name');
 echo ' !';
 ?>
+
+<?php while (have_posts()) : the_post() ?>
+    <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+    <p><?php the_excerpt(); ?></p>
+<?php endwhile; ?>
+
 <?php get_footer(); ?>
