44a9bcb71f50b72499af7acb756ea1b302368048
Yager Guo <guoyg@shinetechchina.com>
2014-08-20 11:33:40 +0800

    输出标签

--- footer.php
+++ footer.php
@@ -1,2 +1,4 @@
+<h3>网站全部 Tag</h3>
+<p><?php wp_tag_cloud(); ?></p>
 </body>
 </html>
--- single.php
+++ single.php
@@ -4,4 +4,7 @@
 <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
 <p> This is single page. <?php the_content(); ?></p>
 
+<h3>Tags</h3>
+<p><?php the_tags(); ?></p>
+
 <?php get_footer(); ?>
